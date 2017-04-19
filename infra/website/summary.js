c94745143b1f41645183b3c8119e346f8082617d
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-19 15:13:44 +0800

    _ wip

--- infra/website/summary.js
+++ infra/website/summary.js
@@ -1,1 +1,18 @@
+let fs = require('fs');
 
+let marked = require('marked');
+
+
+function main () {
+  let a = fs.readFileSync('./CONTRIBUTORS.md', {
+    encoding: 'utf-8',
+  });
+  let b = marked.lexer(a);
+  console.info(b);
+
+  let c = marked.parser(b);
+  console.info(c);
+}
+
+
+main();
